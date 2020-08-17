83b0d8cdc02331b342cdd7a87e604f7ef45ccd04
brinkqiang <brink.qiang@gmail.com>
2020-08-17 11:30:45 +0800

    add

--- include/dmnameof.h
+++ include/dmnameof.h
@@ -1,36 +1,6 @@
-//  _   _                             __    _____
-// | \ | |                           / _|  / ____|_     _
-// |  \| | __ _ _ __ ___   ___  ___ | |_  | |   _| |_ _| |_
-// | . ` |/ _` | '_ ` _ \ / _ \/ _ \|  _| | |  |_   _|_   _|
-// | |\  | (_| | | | | | |  __/ (_) | |   | |____|_|   |_|
-// |_| \_|\__,_|_| |_| |_|\___|\___/|_|    \_____|
-// https://github.com/Neargye/nameof
-// version 0.9.4
-//
-// Licensed under the MIT License <http://opensource.org/licenses/MIT>.
-// SPDX-License-Identifier: MIT
-// Copyright (c) 2016, 2018 - 2020 Daniil Goncharov <neargye@gmail.com>.
-//
-// Permission is hereby  granted, free of charge, to any  person obtaining a copy
-// of this software and associated  documentation files (the "Software"), to deal
-// in the Software  without restriction, including without  limitation the rights
-// to  use, copy,  modify, merge,  publish, distribute,  sublicense, and/or  sell
-// copies  of  the Software,  and  to  permit persons  to  whom  the Software  is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE  IS PROVIDED "AS  IS", WITHOUT WARRANTY  OF ANY KIND,  EXPRESS OR
-// IMPLIED,  INCLUDING BUT  NOT  LIMITED TO  THE  WARRANTIES OF  MERCHANTABILITY,
-// FITNESS FOR  A PARTICULAR PURPOSE AND  NONINFRINGEMENT. IN NO EVENT  SHALL THE
-// AUTHORS  OR COPYRIGHT  HOLDERS  BE  LIABLE FOR  ANY  CLAIM,  DAMAGES OR  OTHER
-// LIABILITY, WHETHER IN AN ACTION OF  CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE  OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
 
-#ifndef NEARGYE_NAMEOF_HPP
-#define NEARGYE_NAMEOF_HPP
+#ifndef __DMNAMEOF_H_INCLUDE__
+#define __DMNAMEOF_H_INCLUDE__
 
 #define NAMEOF_VERSION_MAJOR 0
 #define NAMEOF_VERSION_MINOR 9
@@ -811,4 +781,4 @@ template <typename T>
 #  pragma warning(pop)
 #endif
 
-#endif // NEARGYE_NAMEOF_HPP
+#endif // __DMNAMEOF_H_INCLUDE__
